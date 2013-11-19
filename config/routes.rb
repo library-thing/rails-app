d3004d9d3121196f11c1bbf3bdb0c86dc6318b8c
Charlotte Chang <yellowspiderlove@gmail.com>
2013-11-19 11:43:31 -0500

    home page started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 LibraryThing::Application.routes.draw do
+  resources :books
+
+  root 'books#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

59de0046c54ee9028c3e15b421d7ac093ccd30c5
Charlotte Chang <yellowspiderlove@gmail.com>
2013-11-19 10:13:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LibraryThing::Application.config.secret_key_base = '141eb42ff3753baad99ce7e6b757c4848e8be039d064a88f8bc83228a3e7dc12d4c6e129d516f5aae66f8a4d1f5b66c738b46524a336ce1daaf498c8cc375da9'
